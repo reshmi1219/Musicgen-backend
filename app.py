e58097eda0c93254b0f4a08e1054519fb056c10f
Lode Reshmi <130173975+reshmi1219@users.noreply.github.com>
2026-03-04 13:15:00 +0530

    Change HF_URL and remove HF_TOKEN validation
    Updated Hugging Face API URL and removed HF_TOKEN check.

--- app.py
+++ app.py
@@ -10,10 +10,8 @@ app = Flask(__name__)
 CORS(app)
 
 HF_TOKEN = os.getenv("HF_TOKEN")
-HF_URL = "https://api-inference.huggingface.co/models/facebook/musicgen-small"
 
-if not HF_TOKEN:
-    raise ValueError("HF_TOKEN environment variable not set")
+HF_URL = "https://router.huggingface.co/hf-inference/models/facebook/musicgen-small"
 
 HEADERS = {
     "Authorization": f"Bearer {HF_TOKEN}",
